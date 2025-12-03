f8b8914f8f94e9d27eca33fe85ddcac935fa5a72
lenmanean <lenmanean1@gmail.com>
2025-12-03 09:34:32 -0800

    Fix missing closing div tag in review page FadeInWrapper

--- doer/src/app/onboarding/review/page.tsx
+++ doer/src/app/onboarding/review/page.tsx
@@ -1319,6 +1319,7 @@ export default function ReviewPage() {
               </div>
             </div>
           </div>
+          </div>
         </FadeInWrapper>
 
         {/* Tasks Section with Accordion */}
